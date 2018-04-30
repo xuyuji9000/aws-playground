903af84f68439180194fd6a8bf77444dae00a99d
Karl Xu <karl.yogi.xu@gmail.com>
2018-04-30 13:08:48 +0800

    tf-eb: terraform with elastic beanstalk

--- tf-eb/index.tf
+++ tf-eb/index.tf
@@ -3,23 +3,34 @@ provider "aws" {
   shared_credentials_file = "/Users/yogiman/.aws/credentials"
 }
 
-data "aws_ami" "instance_ami" {
-  most_recent = true
-  # executable_users = ["self"]
+# data "aws_ami" "instance_ami" {
+#   most_recent = true
+#   # executable_users = ["self"]
 
-  filter {
-    name = "name"
-    values = ["amzn-ami-hvm-2018*"]
-  }
-}
+#   filter {
+#     name = "name"
+#     values = ["amzn-ami-hvm-2018*"]
+#   }
+# }
 
 data "aws_elastic_beanstalk_solution_stack" "solution" {
   most_recent = true
   name_regex = "^64bit Amazon Linux (.*) running Docker (.*)$"
 }
 
-output "ami" {
-  value = "${data.aws_ami.instance_ami.id}"
+# output "ami" {
+#   value = "${data.aws_ami.instance_ami.id}"
+# }
+
+resource "aws_elastic_beanstalk_application" "playgound" {
+  name        = "playgound"
+  description = "This is an elasitc beanstalk playgound."
+}
+
+resource "aws_elastic_beanstalk_environment" "node" {
+  name                = "node"
+  application         = "${aws_elastic_beanstalk_application.playgound.name}"
+  solution_stack_name = "${data.aws_elastic_beanstalk_solution_stack.solution.name}"
 }
 
 output "solution" {
